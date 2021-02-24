cd6a2835db0c0dfd37a99227d86a14b83e890f70
KWong41 <46613890+KWong41@users.noreply.github.com>
2021-02-24 13:59:53 -1000

    changed navbar color

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -11,7 +11,7 @@ class NavBar extends React.Component {
   render() {
     const menuStyle = { marginBottom: '10px' };
     return (
-      <Menu style={menuStyle} attached="top" borderless inverted>
+      <Menu style={menuStyle} attached="top" color="blue" borderless inverted>
         <Menu.Item as={NavLink} activeClassName="" exact to="/">
           <Header inverted as='h1'>Local Kine Grinds</Header>
         </Menu.Item>
